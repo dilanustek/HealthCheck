af2e63a95eb32d72bdce79cf23ee0ec6ed5178f6
Dilan <dilanustek@gmail.com>
2020-07-30 15:13:15 -0700

    changed photo confirmation title

--- components/PhotoScreen.js
+++ components/PhotoScreen.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
-import { Dimensions } from 'react-native';
-import { NavigationActions } from 'react-navigation';
-
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+import { Dimensions } from "react-native";
 
 export default class PhotoScreen extends Component {
-  photo = this.props.navigation.getParam('photo');
+  photo = this.props.navigation.getParam("photo");
 
   render() {
     const styles = StyleSheet.create({
       photo: {
-        height: Dimensions.get('window').height,
-        width: Dimensions.get('window').width,
+        height: Dimensions.get("window").height,
+        width: Dimensions.get("window").width,
       },
     });
 
     return (
       <View>
-        <Text> here</Text>
-        <Image style={styles.photo} source={this.photo} ></Image>
-      </View >
-    )
+        <Text> Continue with this photo?</Text>
+        <Image style={styles.photo} source={this.photo}></Image>
+      </View>
+    );
   }
 }
